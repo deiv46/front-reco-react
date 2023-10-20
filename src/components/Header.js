df98ade612cdf0a7bbec117dcbabc8e76b68449a
David Campo Casas <david.campo.casas@gmail.com>
2023-10-20 11:06:16 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
-const Header = ({ username }) => {
+const Header = ({ username, isLoginPage=true }) => {
   return (
     <header className="bg-blue-500 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white font-bold text-xl">Logo de la Empresa</div>
-        {username && (
+        <div className="flex items-center text-white font-bold text-xl">
+          <div className="w-46 h-12">
+            <img
+              src="https://elreferente.es/wp-content/uploads/2021/10/logo-recomotor-1.png"
+              alt="Logo Recomotor"
+              className="w-full h-full"
+            />
+          </div>
+        </div>
+        {!isLoginPage && username && (
           <div className="text-white">
             Usuario Conectado: {username}
           </div>
